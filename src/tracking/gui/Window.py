9f6f749b8bc244ca4add4fd815946f1615497e81
Denis <deniskazberuk@hotmail.com>
2026-02-24 13:46:59 +0100

    ColorPicker in Window (need fix) + start of TrackBall tab

--- src/tracking/gui/Window.py
+++ src/tracking/gui/Window.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import colorchooser
+from tkinter import filedialog
+from tkvideo import tkvideo
 from PIL import Image, ImageTk
 import cv2
+import os
 
+from path import DEFAULT_TRACKING_DIR
 from model.LiveTracking import LiveTracking
 
 class Window():
@@ -30,18 +35,15 @@ class Window():
         # Onglet TrackBall
         self.tabTrackBall = ttk.Frame(self.notebook)
         self.notebook.add(self.tabTrackBall, text="TrackBall")
-        ttk.Label(self.tabTrackBall, text="(TrackBall ici)").pack(pady=20)
+        self._tabTrackBall()
+
+        self.notebook.select(1)
 
     def _tabLive(self):
         # Frame principal
         main = tk.Frame(self.tabLive)
         main.grid(row=0, column=0, sticky="nsew")
 
-        # La fenêtre doit pouvoir s'étirer
-        # self.root.rowconfigure(0, weight=1)
-        # self.root.columnconfigure(0, weight=1)
-
-        # 2 colonnes : gauche (vidéo) plus large, droite (options) plus étroite
         main.columnconfigure(0, weight=0)  # vidéo
         main.columnconfigure(1, weight=0)  # options
         main.rowconfigure(0, weight=0)
@@ -59,28 +61,15 @@ class Window():
         self.labelVideo.pack(fill="both", expand=True)
         self.labelVideo.configure(image= ImageTk.PhotoImage(Image.new("RGBA", self.videoSize, (0, 0, 0, 255))))
 
-        # left.rowconfigure(0, weight=1)
-        # left.columnconfigure(0, weight=1)
-
         # --- Droite : options ---
         right = ttk.Frame(main, padding=10)
         right.grid(row=0, column=1, sticky="nsew")
 
         ttk.Label(right, text="Options", font=("Arial", 14, "bold")).grid(row=0, column=0, sticky="w", pady=(0, 10))
-
         ttk.Label(right, text="Ball Color").grid(row=1, column=0, sticky="w")
 
-        ttk.Label(right, text="R").grid(row=2, column=0, sticky="w")
-        self.inputR = ttk.Entry(right)
-        self.inputR.grid(row=3, column=0, sticky="ew", pady=(0, 8))
-
-        ttk.Label(right, text="G").grid(row=4, column=0, sticky="w")
-        self.inputG = ttk.Entry(right)
-        self.inputG.grid(row=5, column=0, sticky="ew", pady=(0, 8))
-
-        ttk.Label(right, text="B").grid(row=6, column=0, sticky="w")
-        self.inputB = ttk.Entry(right)
-        self.inputB.grid(row=7, column=0, sticky="ew", pady=(0, 12))
+        self.rgbButtons(right)
+        self.colorPickerButtons(right)
 
         ttk.Button(right, text="Start Live", command=self.onStartLive).grid(row=8, column=0, sticky="ew", pady=(0, 6))
         self.labelLive = tk.Label(right, text="OFF", fg="red")
@@ -94,6 +83,77 @@ class Window():
 
         # pour que les widgets de droite prennent toute la largeur
         right.columnconfigure(0, weight=1)
+
+    def rgbButtons(self, parent):
+        rgbButtons = tk.Frame(parent)
+        rgbButtons.grid(row=2, column=0, columnspan=2, sticky="ew")
+
+        labelR = ttk.Label(rgbButtons, text="R")
+        labelR.grid(row=0, column=0, sticky="e")
+        self.inputR = ttk.Entry(rgbButtons, width=4)
+        self.inputR.grid(row=0, column=1, sticky="w", pady=(0, 8))
+
+        labelG = ttk.Label(rgbButtons, text="G")
+        labelG.grid(row=0, column=2, sticky="e")
+        self.inputG = ttk.Entry(rgbButtons, width=4)
+        self.inputG.grid(row=0, column=3, sticky="w", pady=(0, 8))
+
+        labelB = ttk.Label(rgbButtons, text="B")
+        labelB.grid(row=0, column=4, sticky="e")
+        self.inputB = ttk.Entry(rgbButtons, width=4)
+        self.inputB.grid(row=0, column=5, sticky="w", pady=(0, 8))
+
+    def colorPickerButtons(self, parent):
+        cpButtons = tk.Frame(parent)
+        cpButtons.grid(row=3, column=0, columnspan=2, sticky="ew")
+        ttk.Button(cpButtons, text="Choose Color", command=self.onColorPicker).grid(row=0, column=0, sticky="ew", pady=(0, 8))
+
+        ttk.Button(cpButtons, text="✓", command=self.getBGRValues).grid(row=0, column=1, sticky="ew", pady=(0, 8))
+
+    def _tabTrackBall(self):
+        main = tk.Frame(self.tabTrackBall)
+        main.grid(row=0, column=0, sticky="nsew")
+
+        main.columnconfigure(0, weight=0)  # vidéo
+        main.columnconfigure(1, weight=0)  # options
+        main.rowconfigure(0, weight=0)
+        # --- LEFT video ---
+        left = ttk.Frame(main, padding=10)
+        left.grid(row=0, column=0, sticky="nsew")
+
+        videoFrame = tk.Frame(left, width=self.videoSize[0], height=self.videoSize[1], bg="black")
+        videoFrame.grid(row=0, column=0)
+        videoFrame.grid_propagate(False)  # empêche l’auto-resize
+
+        videoPath = "/Users/ahew/Documents/ulb3/projet3/simulation/src/tracking/outputs/live_records/recording_5.mp4"
+        self.labelVideoTrack = tk.Label(videoFrame, bg="black")
+        self.labelVideoTrack.pack(fill="both", expand=True)
+        self.labelVideoTrack.configure(image= ImageTk.PhotoImage(Image.new("RGBA", self.videoSize, (0, 0, 0, 255)))) # random pixels
+
+
+        # ---- RIGHT options ----
+        row = 0
+        right = ttk.Frame(self.tabTrackBall, padding=10)
+        right.grid(row=0, column=1, sticky="nsew")
+
+        ttk.Label(right, text="Options", font=("Arial", 14, "bold")).grid(row=row, column=0, sticky="w", pady=(0, 10))
+        row += 1
+        ttk.Button(right, text="Choose File", command=self.onChooseFile).grid(row=row, column=0, sticky="w", pady=(0, 8))
+        row += 1
+        self.labelFileTrack = tk.Label(right, text="No file selected", fg="red")
+
+        ttk.Label(right, text="Condition initiale").grid(row=row, column=0, sticky="w")
+        row += 1
+        ttk.Label(right, text="Vitesse (unité?):").grid(row=row, column=0, sticky="w", pady=(0, 8))
+        self.inputInitialSpeed = ttk.Entry(right, width=10)
+        self.inputInitialSpeed.grid(row=row, column=1, sticky="w", pady=(0, 8))
+        row += 1
+        ttk.Button(right, text="Track Ball", command=self.getBGRValues).grid(row=row, column=0, sticky="w", pady=(0, 8))
+        row += 1
+
+        ttk.Button(right, text="▶", command=self.onPlayVideoTracked).grid(row=row, column=0, sticky="w", pady=(0, 8))
+        row += 1
+        self.player = tkvideo("/", "void")
 
     def onStartLive(self):
         if self.live:
@@ -137,16 +197,44 @@ class Window():
         self.liveTracking.stopRecording()
         self.labelRecord.configure(text="OFF", fg="red")
 
+    def onColorPicker(self):
+        color = colorchooser.askcolor(title="Choose Ball Color")
+        if color[0] is not None:
+            r, g, b = map(int, color[0])
+            self.inputR.delete(0, tk.END)
+            self.inputR.insert(0, str(r))
+            self.inputG.delete(0, tk.END)
+            self.inputG.insert(0, str(g))
+            self.inputB.delete(0, tk.END)
+            self.inputB.insert(0, str(b))
+
+    def onChooseFile(self):
+        filetypes = ( ('Vidéo', '*.mp4'), ('All files', '*.*') )
+        path = os.path.join(os.getcwd(), DEFAULT_TRACKING_DIR)
+        self.filenameVideoTrack = tk.filedialog.askopenfilename(title='Open a video file', initialdir=path, filetypes=filetypes)
+        if self.filenameVideoTrack:
+            print(f"Selected file: {self.filenameVideoTrack}")
+
+
+    def onPlayVideoTracked(self):
+        if not hasattr(self, 'filenameVideoTrack') or not os.path.isfile(self.filenameVideoTrack):
+            print("No valid video file selected.")
+            return
+        if self.player.live:
+            print("A video is already playing.")
+            return
+        self.player = tkvideo(self.filenameVideoTrack, self.labelVideoTrack, loop=0, size=self.videoSize)
+        self.player.play()
+
     def mainloop(self):
         self.root.mainloop()
 
     def setLiveTracking(self):
         bgrValues = self.getBGRValues()
         if bgrValues is not None:
-            self.liveTracking = LiveTracking(ballColor=bgrValues, videoSource=0, width=self.videoSize[0], height=self.videoSize[1])
+            self.liveTracking = LiveTracking(ballColor=bgrValues, videoSource=1, width=self.videoSize[0], height=self.videoSize[1])
 
     def getBGRValues(self) -> list:
-        return [204, 114, 234]
         try:
             r = int(self.inputR.get())
             g = int(self.inputG.get())
@@ -155,4 +243,4 @@ class Window():
             return [b, g, r]
         except ValueError:
             print("Invalid input for BGR values. Please enter integers.")
-            return None
+        return [53, 92, 112]
