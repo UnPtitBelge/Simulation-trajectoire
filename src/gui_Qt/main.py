8dcc99db514d49aeeb570559b1aa5a075c707853
UnPtitBelge <matteo.morbee@gmail.com>
2026-02-24 20:49:15 +0100

    Main window with 2 tabs (2D and 3D) | Plots are not updated.

--- src/gui_Qt/main.py
+++ src/gui_Qt/main.py
@@ -1,4 +1,12 @@
-from widgets.main_widget import run
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from views.MainWindow import MainWindow
 
 if __name__ == "__main__":
-    run()
+    if __name__ == "__main__":
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec())
